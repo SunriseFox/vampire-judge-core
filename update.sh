1b8483688e03c726d9e44c02b7a6321df4292512
root <root@MSI.localdomain>
2019-04-02 19:51:39 +0800

    fix file permissions

---
+++ update.sh
@@ -0,0 +1,5 @@
+set -ex; \
+  docker start judgecore; \
+  docker cp "judgecore/." judgecore:/usr/bin/judgecore; \
+  docker exec judgecore /bin/chmod +x ./compile.sh; \
+  docker exec judgecore ./compile.sh
