bbef56cb97d6b218044441224a6f9e540ff37efb
SunriseFox <sunrisefox@vampire.rip>
2019-04-11 19:02:15 +0800

    remove some history magic

--- build.sh
+++ build.sh
@@ -9,11 +9,6 @@ MOUNTPOINT="/mnt/data"
 
 set -ex; \
   docker build -t sunrisefox/judgecore:v1.0 .; \
-  docker run --name judgecore --cap-add=SYS_PTRACE --cap-add=SYS_ADMIN --security-opt apparmor=unconfined --security-opt seccomp=unconfined -ti -d -v $MOUNTFOLDER:$MOUNTPOINT --tmpfs /tmp sunrisefox/judgecore:v1.0; \
+  docker run --name judgecore --cap-add=SYS_PTRACE --cap-add=SYS_ADMIN --security-opt apparmor=unconfined --security-opt seccomp=unconfined -ti -d -v $MOUNTFOLDER:$MOUNTPOINT --tmpfs /tmp:exec sunrisefox/judgecore:v1.0; \
 
 # echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-
-
-docker stop judgecore;
-docker rm judgecore;
-docker run --name judgecore --cap-add=SYS_PTRACE --cap-add=SYS_ADMIN --security-opt apparmor=unconfined --security-opt seccomp=unconfined -ti -d -v /var/www/data:/var/www/data --tmpfs /tmp:exec sunrisefox/judgecore:v1.0; \
