20a44ed56d6ed92353d02228c810ebf181cd0a7b
SunriseFox <sunrisefox@vampire.rip>
2019-04-12 19:47:00 +0800

    fix file permission

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 set -ex; \
   docker cp "testcase/." judgecore:/mnt/data; \
+  docker exec judgecore bash -c "find /mnt/data -type d -exec chmod 755 {} \;"  \\
   docker exec judgecore ./judgecore /mnt/data/config/9001-a.json /mnt/data/config/9001-b.json /mnt/data/config/9001-c.json; \
   docker exec judgecore ./compiler /mnt/data/config/spj-9002.json; \
   docker exec judgecore ./judgecore /mnt/data/config/9002-a.json /mnt/data/config/9002-b.json /mnt/data/config/9002-c.json; \
