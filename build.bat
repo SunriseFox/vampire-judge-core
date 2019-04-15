370991aea26dcd7a5bc838073b20e364f4daf748
SunriseFox <sunrisefox@vampire.rip>
2019-04-15 14:18:58 +0800

    restart policy

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@
 @set MOUNTPOINT="/mnt/data"
 
 @docker build -t sunrisefox/judgecore:v1.0 .
-@docker run --name judgecore --cap-add=SYS_PTRACE --cap-add=SYS_ADMIN --security-opt apparmor=unconfined --security-opt seccomp=unconfined -ti -d -v %MOUNTFOLDER%:%MOUNTPOINT% --tmpfs /tmp:exec sunrisefox/judgecore:v1.0
+@docker run --name judgecore --cap-add=SYS_PTRACE --cap-add=SYS_ADMIN --security-opt apparmor=unconfined --security-opt seccomp=unconfined -ti -d -v %MOUNTFOLDER%:%MOUNTPOINT% --tmpfs /tmp:exec --restart unless-stopped sunrisefox/judgecore:v1.0
 
 @REM @docker run --name judgecore --cap-add=SYS_PTRACE --cap-add=SYS_ADMIN --security-opt apparmor=unconfined --security-opt seccomp=unconfined -ti -d -v "D:\0bysj\core":/mnt/core -v "D:\0bysj\volume":/mnt/data sunrisefox/judgecore:v1.0
